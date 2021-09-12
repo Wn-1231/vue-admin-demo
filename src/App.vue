a05210d29afb6ab593bc99f0e1353425cdfb3e39
wangning <wangning@chunyu.me>
2021-09-12 12:26:09 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<!--
+ * @Author: wn
+ * @Date: 2021-01-18 00:08:58
+ * @LastEditTime: 2021-06-17 13:52:20
+ * @LastEditors: wn
+ * @Description: 
+-->
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <div>
+    {{ a }}
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+  setup() {
+    return {
+      a: 1,
+    };
+  },
+};
+</script>
